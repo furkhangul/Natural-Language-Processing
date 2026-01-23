7033a5a909c803d4465d1722660ab93a4170a0be
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2026-01-23 23:29:27 +0300

    2

---
+++ 0.2 Kütüphaneler ve Amaçları.py
@@ -0,0 +1,28 @@
+#NLP Kütüphaneleri ve Kullanım Alanları
+
+import nltk
+"""
+NLTK yani Natural Language ToolKit kütüphanesi istatistiksel ve 
+sebmobil DDI ifadeler için kurulmuştur.
+Bir çok doğal dil işleme kütüpühanesinin temelini oluşturur ve yer alır.
+
+
+pip3 install  --user -U nltk 
+şeklinde indirebiliriz. 
+Veya IDE üzerinden de ctrl+alt+s ile de indirebiliriz.
+"""
+
+import scapy
+
+"""
+Açık kaynaklı koddur ve genellikle sinir ağları üzerinde kullanılır. 
+"""
+
+import zemberek
+
+"""
+Türkçe kaynalıklı doğal dil işleme kütüphanesidir.
+Java üzerinden yazılmış ve açık kaynak kodludur.
+
+
+"""
