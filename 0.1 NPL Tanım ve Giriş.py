af890f26791d5d585f87f3b71d285d73dd8dd459
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2026-01-23 14:16:29 +0300

    1

---
+++ 0.1 NPL Tanım ve Giriş.py
@@ -0,0 +1,57 @@
+#Doğal Dil İşleme
+
+"""
+Doğal Dil İşleme Nedir ?
+Bilgisayarın insan dilini anlamasını yorumlamasını ve üretimesini sağlayan
+yapay zeka alanıdır.
+
+İnsan konuşur ve makine bu konuşmayı bir insan gibi algılamaya çalışır.
+
+--->Doğa diller alfabelerden oluşurken, bilgisayar dilleri 0 ve 1 den oluşur.
+NLP dilin bilgisayar diline çevrilmesini sağlar.
+
+Kullanım Alanları Nelerdir ?
+-Makine çevirisi: Kısaca translateler diyebiliriz.
+-Konuşma tanıma: Sesi text şekline çevirebilmeyi hedefler
+-Hissiyat Analizi: Jest ve mimikleri göstermek için kelimelerin anlamsal olarak
+duygusal mesajları ve anlamlarını çıkartır.
+-Soru Cevaplama: Sorulan soruya doğru bir şekilde cevap vermesi diyebiliriz.
+
+
+
+Alt Başlıkları Nelerdir?
+
+1-Kelime Bilirimi:
+Bir dildeki sözcüklerin kökenlerini, o dilin diğer dillere ve topluluklara
+olan kültürel ilişkisini araştırır.
+
+2-Söz Dizimi:
+Doğal dildeki cümle kurma ilke ve kurallarını cümlelerin esnekliğini
+inceleyen dil bilimi dalıdır.
+
+3-Anlamsal Analiz:
+Semantik yani anlam bilimidir. Sözcüğün anlamını ve cümlenin anlamını çıkarım
+yaparak anlamaya çalışır.
+Mesela aynı sözcük farklı anlamlara gelebilir.
+
+4-Anlam Belirsizliği
+Kelimenin anlamı ve bu anlamın bağlamı bulunur. Farklı bağlamlardan
+farklı anlam çıkarbilirmesi gerekir.
+
+
+NPL'in Araştırma Konuları Nelerdir ?
+1-Varlık İsmi Tanıma
+Metinde yer alan özel isimleri, saat, tarih, lokasyon, kişi ve kurum
+adlarını anlamlandırılması işlemidir.
+
+2-Özetleme
+Çıkarıcı Özetleme: Metinde yer alan kelime ve cümleler
+Soyutlayıcı Özetleme: Metinde yer almayan kelime ve cümleler
+
+3-Metin Normalizasyonu
+Yazım hataları ve bu hataların kasti olup olmadığını anlama. Bunların sonucunda
+analizler için yüksek başarı oranı sunulmasıdır.
+
+4-Metin Sınıflandırma
+Anlam ve uzunluklarına göre sınıflandırmasını sağlar. 
+"""
